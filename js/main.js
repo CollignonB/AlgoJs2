e28d86d59dafe92cb54e76c5224741e881fc6f51
Baptiste COLLIGNON <collignon@CDI>
2020-07-07 16:37:38 +0200

    ex7 done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-let mesCourses = [
-    "Pains",
-    "Nutella",
-    "Jambon",
-    "Yahourt",
-    "Pâtes complètes",
-    "Bacon",
-    "Steak",
-    "Pain à burger",
-    "Sauces"
-]
+let personalDatas = [
+    {
+        Nom : "Collignon",
+        Prenom : "Baptsite",
+        Age : 24,
+        DateDeNaissance : "15/09/1995"
+    },
+    {
+        Nom : "Zblah",
+        Prenom : "Jean-Charles",
+        Age : 64,
+        DateDeNaissance : "15/09/1956"
+    }
+];
 
-function oubliePas(course){
-    let dernier = course.length - 1;
-    console.log(`oublies pas les ${course[0]} , les ${course[dernier]} et les ${course[4]} !`);
+function idCard(datas){
+    for (const person of datas) {
+        for (const property in person) {
+            console.log(`${property}: ${person[property]}`);
+        }
+        console.log("---------------");
+    }
 }
-
-oubliePas(mesCourses);
+idCard(personalDatas);
