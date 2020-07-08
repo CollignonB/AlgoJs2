6dc4c338c7f1c95693dba29f1f9502a60a92e7d8
Baptiste COLLIGNON <collignon@CDI>
2020-07-08 09:28:54 +0200

    frist commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,1 @@
-var chaine = "";
-for (var i = 1; i < 10; i ++){
-    if ( i % 2 === 0){
-        for(var j = 0; j < 17; j ++){
-            (j % 2 !== 0 ? chaine += "#" : chaine += " " );
-        }
-        chaine +="\n";
-    }else{
-        for(var j = 0; j < 17; j ++){
-            (j % 2 === 0 ? chaine += "#" : chaine += " " );
-        }
-        chaine +="\n";
-    }
-}
-console.log(chaine);
+// voir les autres branches pour les exercices
