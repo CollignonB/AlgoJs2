910907a2870aa318363477d41c6863b4dc86f3d3
Baptiste COLLIGNON <collignon@CDI>
2020-07-08 09:03:29 +0200

    ex9 done

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
-let tableNum = [1,2,5,7,684512,23,0,654,8];
-let tableStrings = ["sbfg","wzexrctubyiregfdghsdf", "zgdnnh", "ugfmlkdfblpns","za"];
+var inputUser = [];
+let i = 0;
+do {
+    inputUser.push(prompt("cc"));
+}while(!inputUser.includes("fin"));
 
-function bigger (table){
-    let isBigger = 0;
-    if (isStringArray(table)){
-        let isBiggerChain = "";
-        for (const value of table) {
-            if(value.length > isBiggerChain.length){
-                isBiggerChain = value;
-            }
-        }
-        return isBiggerChain;
-    }else if(isIntArray(table)){
-        for (const value of table) {
-            if(value > isBigger){
-                isBigger = value;
-            }
-        }
-        return isBigger;
-    }
-}
-
-function isIntArray(table){
-    for (const value of table) {
-        return (typeof(value) !== "number") ? false : true;
-    }
-}
-
-function isStringArray(table){
-    for (const value of table) {
-        return (typeof(value) !== "string") ? false : true;
-    }
-}
-
-console.log(bigger(tableStrings));
+inputUser.pop();
+console.log(inputUser);
